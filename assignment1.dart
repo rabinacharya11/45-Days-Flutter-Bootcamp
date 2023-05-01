c966a4e8a65455c213ef051feac158256fea49f0
manishadong <130970555+manishadong@users.noreply.github.com>
2023-05-01 21:32:47 +0545

    upload assignment

---
+++ assignment1.dart
@@ -0,0 +1,17 @@
+//Assignment1
+void main() {
+  int mynumber = 42;
+  {
+    print(mynumber);
+  }
+
+  String myString = "Hello world";
+  {
+    print(myString);
+  }
+
+  bool myBoolean = true;
+  {
+    print(myBoolean);
+  }
+}
---
+++ assignment2.dart
@@ -0,0 +1,10 @@
+void main() {
+  var mynumber = 133;
+  print(mynumber.runtimeType);
+
+  var myString = "HSM";
+  print(myString.runtimeType);
+
+  var myboolean = true;
+  print(myboolean.runtimeType);
+}
