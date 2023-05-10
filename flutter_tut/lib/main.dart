8d4f9b790034b71b672aedb3e7573a8f252de13d
Rabin Acharya <charms2074@gmail.com>
2023-05-10 20:44:47 +0545

    text widget done

--- flutter_tut/lib/main.dart
+++ flutter_tut/lib/main.dart
@@ -31,25 +31,19 @@ class _HomeScreenState extends State<HomeScreen> {
         child: Container(
           height: 400,
           width: 300,
-          // color: Colors.red,
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Colors.red,
-                Colors.blue,
-                Colors.white,
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(500),
-              bottomLeft: Radius.circular(500),
-              topRight: Radius.circular(500),
-              bottomRight: Radius.circular(500),
+          child: Center(
+            child: Text(
+              "Hello",
+              textAlign: TextAlign.start,
+              style: TextStyle(
+                color: Colors.indigo,
+                fontSize: 30,
+                fontWeight: FontWeight.w900,
+                decoration: TextDecoration.overline,
+                letterSpacing: 10,
+              ),
             ),
           ),
-          child: Center(child: Text("Hello")),
         ),
       ),
     );
