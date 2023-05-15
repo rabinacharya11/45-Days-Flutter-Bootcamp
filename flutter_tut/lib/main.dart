c44972dd21fc3034da4fec5de33f85ebdecf8ec7
Rabin Acharya <charms2074@gmail.com>
2023-05-15 21:16:55 +0545

    image, textField, rich text, progess Indicator

--- flutter_tut/lib/main.dart
+++ flutter_tut/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,7 +10,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: HomeScreen(),
     );
@@ -17,41 +18,130 @@ class MyApp extends StatelessWidget {
 }
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
+  HomeScreen({super.key});
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  void signIn({required String email, required String password}) {
+    if (email == "test@gmail.com" && password == 'test@1101') {
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text("You are logged in")));
+    } else {
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text("You are not logged in")));
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        // drawer: Drawer(),
-        // appBar: AppBar(),
+        appBar: AppBar(),
         body: SafeArea(
-      child: SizedBox(
-        height: MediaQuery.of(context).size.height,
-        width: MediaQuery.of(context).size.width,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Container(
-              height: 200,
-              child: CircleAvatar(
-                minRadius: 100,
-                maxRadius: 300,
-                backgroundColor: Colors.amber,
-                backgroundImage: NetworkImage(
-                    "https://media.istockphoto.com/id/629022568/photo/branching-moonlight.jpg?s=612x612&w=0&k=20&c=pECtItfnKuOC-RIzGXk1tQfzWSetMEmwiQCX5msooxg="),
-              ),
-            ), 
-
-            
-          ],
-        ),
-      ),
-    ));
+          child: SizedBox(
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    'assets/img.png',
+                    height: 400,
+                    width: 300,
+                  ),
+                  Text(
+                    "Sign In ",
+                  ),
+                  Text(
+                    "You can access Fingerprint \nto to grant access to the app Rs. \$100",
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 40.0,
+                    ),
+                    child: TextField(
+                      controller: emailController,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(color: Colors.red),
+                      decoration: InputDecoration(
+                        hintText: 'Email Address',
+                        hintStyle: TextStyle(
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 40.0,
+                    ),
+                    child: TextField(
+                      controller: passwordController,
+                      // obscureText: true,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(color: Colors.red),
+                      decoration: InputDecoration(
+                        hintText: 'Password',
+                        hintStyle: TextStyle(
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ),
+                  ),
+                  InkWell(
+                    onTap: () {
+                      signIn(
+                          email: emailController.text.trim(),
+                          password: passwordController.text.trim());
+                      // ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                      //     content: Text(
+                      //         'Email : ${emailController.text} \n Password: ${passwordController.text}')));
+                    },
+                    child: Container(
+                      height: 40,
+                      width: 200,
+                      color: Colors.blue,
+                      child: Center(
+                          child: Text(
+                        "Log In",
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      )),
+                    ),
+                  ),
+                  CircularProgressIndicator.adaptive(),
+                  LinearProgressIndicator(),
+                  Text(
+                    "Forgot Password?",
+                    style: TextStyle(
+                      color: Colors.blue,
+                      decoration: TextDecoration.underline,
+                    ),
+                  ),
+                  RichText(
+                    text: TextSpan(
+                        text: "Create Account?",
+                        style: TextStyle(color: Colors.grey),
+                        children: [
+                          TextSpan(
+                            text: "Create One",
+                            recognizer: TapGestureRecognizer()
+                              ..onTap = () {
+                                print("Print accoubt crrating");
+                              },
+                            style: TextStyle(color: Colors.blue),
+                          ),
+                        ]),
+                  )
+                ]),
+          ),
+        ));
   }
 }
